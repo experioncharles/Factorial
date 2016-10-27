54df81eff9774826adc249f4ffee63b3dd60d39c
experioncharles <charles.sebastian@experionglobal.com>
2016-10-27 13:29:24 +0530

    Charles

--- factorial.java
+++ factorial.java
@@ -1,13 +1,16 @@
 import java.io*
 
+
 Class Factorial{
 	public static void main(String args[])
 	{
-		int i,fact=1,num=5;
+		int i,fact=1,n;
 		System.out.println("Input the value");
-		for(i=1;i<num;i++)
+		n = Integer.parseInt(System.console().readLine());
+		for(i=1;i<n;i++)
 		{
 			fact=fact*i;
 		}
 		System.out.println("Factorial is",+fact);
 	}
+}
